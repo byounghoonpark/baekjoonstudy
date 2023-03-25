58679144a7537b7275b0c8b54be7bdfa0aeb80fb
byounghoonpark <115392073+byounghoonpark@users.noreply.github.com>
2023-03-25 23:03:21 +0900

    [Unrated] Title: A/B, Time: 224 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Unrated/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+public static void main(String[]args){
+Scanner S = new Scanner(System.in);
+double A = S.nextDouble();
+double B = S.nextDouble();
+System.out.println(A/B);
+}
+}
