29206fecfe146d8cd45a35b11e9f3d20dd896a9e
byounghoonpark <115392073+byounghoonpark@users.noreply.github.com>
2023-03-25 23:24:26 +0900

    [Unrated] Title: ??!, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Unrated/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner S = new Scanner(System.in);
+        String A = S.next();
+        String B = "??!";
+        System.out.println(A+B);
+    }
+}
