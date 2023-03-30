69a831eccd561c6ba1ca6df126c10a08b2f354cc
byounghoonpark <115392073+byounghoonpark@users.noreply.github.com>
2023-03-30 16:28:10 +0900

    Update 과제 안 내신 분．．？.py

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -3,7 +3,13 @@ for i in range(30):
     test.append(i+1)
 for _ in range(28):
     n = int(input())
+    
+# remove()함수로 리스트 원소 직접 삭제
+# del test[n] 이렇게 index로도 삭제 가능
     test.remove(n)
+    
+# 오름차순 정렬 == sort(reverse=False)
+# sort(reverse=True) 이렇게 쓰면 내림차순 정렬 가능
 test.sort()
 print(test[0])
 print(test[1])
