d343fb897a89a6f1b7ffe75fc0349743c9abae16
byounghoonpark <115392073+byounghoonpark@users.noreply.github.com>
2023-04-11 18:11:59 +0900

    [Unrated] Title: 삼각형 외우기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+z = int(input())
+if x+y+z == 180:
+    if x and y and z == 60:
+        print('Equilateral')
+    elif x == y or y == z or z == x :
+        print('Isosceles')
+    else: print('Scalene')
+else: print('Error')
